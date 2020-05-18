6014e6d6d4f5139c137ee1a7bf0185f30272527d
Djalma <juninho_2509@hotmail.com>
2020-05-18 10:19:38 -0300

    estrutura inicial

---
+++ OnlineBS/OnlineBS.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnlineBS.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ OnlineBS/OnlineBS.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnlineBS.Infra
+{
+    public class Class1
+    {
+    }
+}
